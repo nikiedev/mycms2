711cd2e5dc666b20e4cba4e74273d86963b91d9a
nikiedev <y5fJ78X4gbi29d1Lnk>
2024-03-11 19:36:22 +0200

    file updated

--- saved.php
+++ saved.php
@@ -1,3 +1,4 @@
+test
 <div class="rating" data-rating="<?= $item->rating; ?>">
 	<?php for ($i = 5; $i > 0; $i--): ?>
 		<input type="radio" name="rating" id="r<?= $i; ?>" value="<?= $i; ?>" <?= $item->rating == $i ? ' checked' : '' ?>>
